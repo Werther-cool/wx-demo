0de4c520c5732c8213a72f1367ade836251f60f6
Werther <zhouwangyulu@icloud.com>
2017-07-27 09:01:58 +0800

    wx

---
+++ wx-now/page/component/index.js
@@ -0,0 +1,73 @@
+//获取应用实例
+var app = getApp()
+Page({
+	data: {
+    list: [
+      {
+        id: 'view',
+        name: '视图容器',
+        open: false,
+        pages: ['view', 'scroll-view', 'swiper']
+      }, {
+        id: 'content',
+        name: '基础内容',
+        open: false,
+        pages: ['text', 'icon', 'progress']
+      }, {
+        id: 'form',
+        name: '表单组件',
+        open: false,
+        pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'radio', 'slider', 'switch', 'textarea']
+      }, {
+        id: 'nav',
+        name: '导航',
+        open: false,
+        pages: ['navigator']
+      }, {
+        id: 'media',
+        name: '媒体组件',
+        open: false,
+        pages: ['image', 'audio', 'video']
+      }, {
+        id: 'map',
+        name: '地图',
+        pages: ['map']
+      }, {
+        id: 'canvas',
+        name: '画布',
+        pages: ['canvas']
+      }
+    ]
+	},
+
+	//事件处理函数
+	bindViewTap: function() {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	onLoad: function() {
+		console.log('onLoad')
+		var that = this
+		//调用应用实例的方法获取全局数据
+		app.getUserInfo(function(userInfo) {
+			//更新数据
+			that.setData({
+				userInfo: userInfo
+			})
+		})
+	},
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
+  }
+})
